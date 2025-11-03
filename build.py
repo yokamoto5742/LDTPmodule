a429f01c63d9ba2b06d3fda1eb5e930920051f5f
yokamoto5742-h <yokamoto5742@gmail.com>
2025-11-03 15:24:03 +0900

    ✨ chore(app): バージョンと日付を更新

--- build.py
+++ build.py
@@ -1,27 +1,21 @@
 import subprocess
-import os
-import shutil
-from version_manager import update_version, update_version_py
+
+from scripts.version_manager import update_version
 
 
 def build_executable():
     new_version = update_version()
-    update_version_py(new_version)
-
     subprocess.run([
         "pyinstaller",
         "--name=LDTPapp",
         "--windowed",
         "--icon=assets/LDPTapp_icon.ico",
+        "--add-data", "utils/config.ini:.",
         "main.py"
     ])
 
-    dist_path = os.path.join('dist', 'LDTPapp')
-
-    if not os.path.exists(os.path.join(dist_path, 'config.ini')):
-        shutil.copy('utils/config.ini', dist_path)
-
     print(f"Executable built successfully. Version: {new_version}")
+    return new_version
 
 
 if __name__ == "__main__":
