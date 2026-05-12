b47f1a85f63b01b2e94ce0de00eed6dc35fc3bba
yokamoto5742-h <yokamoto5742@gmail.com>
2026-05-12 19:17:12 +0900

    ✨ feat(version): バージョンを1.0.0に更新し、日付を最新化

--- build.py
+++ build.py
@@ -1,10 +1,7 @@
 import subprocess
 
-from scripts.version_manager import update_version
-
 
 def build_executable():
-    new_version = update_version()
     subprocess.run([
         "pyinstaller",
         "--name=LDTPapp",
@@ -14,8 +11,7 @@ def build_executable():
         "main.py"
     ])
 
-    print(f"Executable built successfully. Version: {new_version}")
-    return new_version
+    print(f"Executable built successfully.")
 
 
 if __name__ == "__main__":
